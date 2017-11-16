8069c6cc03307ed8aeb4cb97043728c09f1c0117
Yekaterina <katia.shukh@gmail.com>
2017-11-16 10:20:49 -0800

    first commit

--- main/apps/rand_word/urls.py
+++ main/apps/rand_word/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    # url(r'^generate$', views.generate),
+    url(r'^generate$', views.generate),
     # url(r'^reset$', views.reset),
 ]
--- main/apps/rand_word/views.py
+++ main/apps/rand_word/views.py
@@ -8,11 +8,28 @@ from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
-    
     context={
         "mystr" : get_random_string(14)
     }
+    try:
+        request.session['tries']
+    except KeyError:
+        request.session['tries']=0
+
     return render(request,'rand_word/index.html', context)
+
+# IF WE RETURN JUST THE RAW TEMPLATE FILE, THE 'context' dictionary in the "index" method
+# is not defined.
+# IF WE RETURN REDIRECT to the ROUTE (root route) that calls 'Index' method,
+# the 'context' dictionary again gets generated and we see result on the page.
+ 
+def generate(request):
+    request.session['tries'] += 1
+    request.session['word']= get_random_string(14)
+    # return render(request, 'rand_word/index.html')
+    return redirect('/')
+
+
 
 # Example: get_random_string(length=14) gets a random string
 # of legth 14. 
